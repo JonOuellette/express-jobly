b225ae954a1fa9bb2ce5d01c74c555aac845350a
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-12-10 15:28:35 -0800

    updated user model and routes to incorporate a user to apply for a job.

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -39,6 +39,11 @@ async function commonBeforeAll() {
         await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
         await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
       ]);
+
+  await db.query(`
+      INSERT INTO applications(username, job_id)
+      VALUES ('u1', $1)`,
+    [testJobIds[0]]);
 }
 
 async function commonBeforeEach() {
--- routes/users.test.js
+++ routes/users.test.js
@@ -222,6 +222,7 @@ describe("GET /users/:username", function () {
         applications: [testJobIds[0]],
       },
     });
+  });
   
   test("unauth for other users", async function () {
     const resp = await request(app)
@@ -378,5 +379,38 @@ describe("DELETE /users/:username", function () {
         .delete(`/users/nope`)
         .set("authorization", `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(404);
+  });
+});
+
+/************************************** applyToJob */
+
+describe("applyToJob", function () {
+  test("works", async function () {
+    await User.applyToJob("u1", testJobIds[1]);
+
+    const res = await db.query(
+        "SELECT * FROM applications WHERE job_id=$1", [testJobIds[1]]);
+    expect(res.rows).toEqual([{
+      job_id: testJobIds[1],
+      username: "u1",
+    }]);
+  });
+
+  test("not found if no such job", async function () {
+    try {
+      await User.applyToJob("u1", 0, "applied");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+
+  test("not found if no such user", async function () {
+    try {
+      await User.applyToJob("nope", testJobIds[0], "applied");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
   });
 });
