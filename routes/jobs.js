ad103b6e6d05672921134c674b9c1a4d26119667
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-12-10 13:28:03 -0800

    build: Created json Schemas to create new jobs, update existing jobs and search jobs.

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,4 +3,7 @@
 /** Routes for jobs */
 
 const jsonschema = require("jsonschema");
-const express = require("express")
+const express = require("express");
+const { BadRequestError } = require("../expressError");
+const { ensureAdmin } = require("../middleware/auth");
+const Job = require("../models/jobs");
